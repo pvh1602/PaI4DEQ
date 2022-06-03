9f95f51cb2632b1df10bdc8b79264d55921ea162
pvh1602 <pvh16021998@gmail.com>
2022-06-03 13:53:49 +0700

    update

--- scripts/resnet_deq/run_1.sh
+++ scripts/resnet_deq/run_1.sh
@@ -5,7 +5,7 @@ for pruner in rand
 do
     for sparsity in 0 0.1 0.5 0.7 0.9 0.95 0.97 0.99 0.999
     do
-        CUDA_VISIBLE_DEVICES=0 python resnet_deq.py --is_pruning --sparsity $sparsity --pruner $pruner
+        CUDA_VISIBLE_DEVICES=0 python resnet_deq.py --is_pruning --sparsity $sparsity --pruner $pruner --is_wandb
     done
 
 done
@@ -14,7 +14,7 @@ for pruner in mag
 do
     for sparsity in 0.1 0.5 0.7 0.9 0.95 0.97 0.99 0.999
     do
-        CUDA_VISIBLE_DEVICES=0 python resnet_deq.py --is_pruning --sparsity $sparsity --pruner $pruner
+        CUDA_VISIBLE_DEVICES=0 python resnet_deq.py --is_pruning --sparsity $sparsity --pruner $pruner --is_wandb
     done
 
 done
