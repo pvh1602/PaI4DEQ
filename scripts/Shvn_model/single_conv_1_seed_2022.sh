8cfe7afe7dc37c68364950e719d926c45bbc3668
pvh1602 <pvh16021998@gmail.com>
2022-06-03 20:09:45 +0700

    update scripts

--- scripts/Shvn_model/single_conv_1_seed_2022.sh
+++ scripts/Shvn_model/single_conv_1_seed_2022.sh
@@ -1,6 +1,15 @@
 cd ../..
 
-for pruner in rand mag 
+for pruner in rand
+    do 
+    for sparsity in 0.97 0.99 0.999
+    # for sparsity in 0.9 0.95 0.97 0.99 0.999
+        do
+            CUDA_VISIBLE_DEVICES=0 python run_exps.py --model SingleConvNet --pruner $pruner --sparsity $sparsity --dataset shvn --is_wandb --seed 2022 --is_pruning --lr 0.001 --lr_drop_step 25
+        done
+    done
+
+for pruner in mag 
     do 
     for sparsity in 0.1 0.5 0.7 0.9 0.95 0.97 0.99 0.999
     # for sparsity in 0.9 0.95 0.97 0.99 0.999
--- scripts/Shvn_model/single_conv_2_seed_2022.sh
+++ scripts/Shvn_model/single_conv_2_seed_2022.sh
@@ -1,6 +1,15 @@
 cd ../..
 
-for pruner in snip grasp 
+for pruner in snip 
+    do 
+    for sparsity in 0.97 0.99 0.999
+    # for sparsity in 0.9 0.95 0.97 0.99 0.999
+        do
+            CUDA_VISIBLE_DEVICES=0 python run_exps.py --model SingleConvNet --pruner $pruner --sparsity $sparsity --dataset shvn --is_wandb --seed 2022 --is_pruning --lr 0.001 --lr_drop_step 25
+        done
+    done
+
+for pruner in grasp 
     do 
     for sparsity in 0.1 0.5 0.7 0.9 0.95 0.97 0.99 0.999
     # for sparsity in 0.9 0.95 0.97 0.99 0.999
