b28406bb5ca66a55f4872c3c2a00ab1060abf56a
pvh1602 <pvh16021998@gmail.com>
2022-06-03 16:57:03 +0700

    update scripts

--- scripts/Cifar_small_model/ablation_1.sh
+++ scripts/Cifar_small_model/ablation_1.sh
@@ -1,7 +1,7 @@
 
 cd ../..
 
-CUDA_VISIBLE_DEVICES=2 python run_exps.py --is_ablation --ablation_mode 0 --dataset cifar10 --model MultiConvNet --sp PR --is_pruning --is_wandb --seed 1 --lr 0.01
-CUDA_VISIBLE_DEVICES=2 python run_exps.py --is_ablation --ablation_mode 1 --dataset cifar10 --model MultiConvNet --sp PR --is_pruning --is_wandb --seed 1 --lr 0.01
-CUDA_VISIBLE_DEVICES=2 python run_exps.py --is_ablation --ablation_mode 2 --dataset cifar10 --model MultiConvNet --sp PR --is_pruning --is_wandb --seed 1 --lr 0.01
-CUDA_VISIBLE_DEVICES=2 python run_exps.py --is_ablation --ablation_mode 3 --dataset cifar10 --model MultiConvNet --sp PR --is_pruning --is_wandb --seed 1 --lr 0.01
+CUDA_VISIBLE_DEVICES=3 python run_exps.py --is_ablation --ablation_mode 0 --dataset cifar10 --model MultiConvNet --sp PR --is_pruning --is_wandb --seed 2 --lr 0.01
+CUDA_VISIBLE_DEVICES=3 python run_exps.py --is_ablation --ablation_mode 1 --dataset cifar10 --model MultiConvNet --sp PR --is_pruning --is_wandb --seed 2 --lr 0.01
+CUDA_VISIBLE_DEVICES=3 python run_exps.py --is_ablation --ablation_mode 2 --dataset cifar10 --model MultiConvNet --sp PR --is_pruning --is_wandb --seed 2 --lr 0.01
+CUDA_VISIBLE_DEVICES=3 python run_exps.py --is_ablation --ablation_mode 3 --dataset cifar10 --model MultiConvNet --sp PR --is_pruning --is_wandb --seed 2 --lr 0.01
