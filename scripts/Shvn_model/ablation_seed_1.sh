14188d1d3d41827a47c41d40f9e41b30310d82f1
pvh1602 <pvh16021998@gmail.com>
2022-06-05 23:12:50 +0700

    add shvn ablation scripts

---
+++ scripts/Shvn_model/ablation_seed_1.sh
@@ -0,0 +1,7 @@
+
+cd ../..
+
+CUDA_VISIBLE_DEVICES=0 python run_exps.py --is_ablation --ablation_mode 0 --dataset shvn --model SingleConvNet --sp PR --is_pruning --is_wandb --seed 1  --lr 0.001 --lr_drop_step 25
+CUDA_VISIBLE_DEVICES=0 python run_exps.py --is_ablation --ablation_mode 1 --dataset shvn --model SingleConvNet --sp PR --is_pruning --is_wandb --seed 1  --lr 0.001 --lr_drop_step 25
+CUDA_VISIBLE_DEVICES=0 python run_exps.py --is_ablation --ablation_mode 2 --dataset shvn --model SingleConvNet --sp PR --is_pruning --is_wandb --seed 1  --lr 0.001 --lr_drop_step 25
+CUDA_VISIBLE_DEVICES=0 python run_exps.py --is_ablation --ablation_mode 3 --dataset shvn --model SingleConvNet --sp PR --is_pruning --is_wandb --seed 1  --lr 0.001 --lr_drop_step 25
